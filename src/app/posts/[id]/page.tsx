87e5dcbb8d1378af6c2f7b3b6990019542326b76
Kashaf Naz <kashafnazk389@gmail.com>
2025-01-03 15:49:49 +0500

    Add files via upload

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,7 @@
  
 import React from 'react';
 import Link from 'next/link';
+
 type PageProps = {
   params: {
     id: string;
@@ -11,17 +12,24 @@ type PageProps = {
   };
 };
 
-const Page: React.FC<PageProps> = async ({ params, searchParams }) => {
-  const { id } = await  params;
-  const { title , body } = await searchParams;
-    return (
-      <div className='w-[80%] mx-auto p-11'>
-          <h1 className=' my-5 text-[35px] font-mono'>Post ID: {id}</h1>
-          <h2><b className='text-[20px] font-serif'>Title:</b> {title}</h2>
-          <p><b className='text-[20px] font-serif'>Body:</b>{body}</p>
-          <Link href="/" ><button className='p-[10px] my-5 text-[white] bg-black rounded-md'>Veiw all Posts</button></Link>
-      </div>
-    );
-}
+const Page: React.FC<PageProps> = ({ params, searchParams }) => {
+  const { id } = params; 
+  const { title, body } = searchParams;
+
+  return (
+    <div className="w-[80%] mx-auto p-11">
+      <h1 className="my-5 text-[35px] font-mono">Post ID: {id}</h1>
+      <h2>
+        <b className="text-[20px] font-serif">Title:</b> {title}
+      </h2>
+      <p>
+        <b className="text-[20px] font-serif">Body:</b> {body}
+      </p>
+      <Link href="/">
+        <button className="p-[10px] my-5 text-white bg-black rounded-md">View all Posts</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Page;
